374d071804665a7cf240a6b693cc83c30fa42967
butlersrepos <ericbutler000@gmail.com>
2025-01-27 22:44:42 -0500

    Fixed cycle state box building
    Fixed so that cycles with start and end build correctly even if they have directionality to them.
    Tweaked animation tree node spacing in editor.

--- AnimationBuilders/MiniFantasyAnimationTreeBuilder.gd
+++ AnimationBuilders/MiniFantasyAnimationTreeBuilder.gd
@@ -9,6 +9,7 @@ func _run() -> void:
 	FileOps.save_node(tree)
 
 const BLEND_POSITIONS = {
+	"none": Vector2.ZERO,
 	"up": Vector2(0, -1),
 	"right": Vector2(1, 0),
 	"down": Vector2(0, 0.9), # Slight offset to favor facing down(camera) while moving horizontally
@@ -53,29 +54,37 @@ static func create_animation_tree(character: String, library: AnimationLibrary) 
 	# Track and place the nodes in two clusters, one for all the standardly connected ones, and an area with the rest left up to the user
 	var standard_nodes_index = 0
 	var other_nodes_index = 0
-	var node_width = longest_name * 12
+	# Loose approximation to space the nodes in the editor so they don't overlap names
+	var node_width = longest_name * 18
 	
 	var state_machine = AnimationNodeStateMachine.new()
 	state_machine.set_node_position('Start', Vector2(-100, -100))
 	state_machine.set_node_position('End', Vector2(100, -100))
 	# Programmatically build all states and assign their animations based on what directions they seem to support
 	for action: String in anim_infos:
-		var anim_info = anim_infos[action][0]
-		var action_blend_space = create_blend_space()
+		# Check for multi-animation sets like foo-start, foo, foo-end. Which can also have directionality
+		var like_actions = anim_infos[action]
+		var main_anims = like_actions.filter(func(i): return !i['is_start'] and !i['is_end'])
+		var leave_anims = like_actions.filter(func(i): return i['is_end'])
+		var enter_anims = like_actions.filter(func(i): return i['is_start'])
+
+		## Special case for idle, which can have activate/deactivate anims so we build a state machine instead of a single blendspace
+		## This simplifies connecting transitions to it later
+		if (action == 'idle') and anim_infos.keys().has('activate') and anim_infos.keys().has('deactivate'):
+			var start_actions = anim_infos['activate']
+			var idle_actions = anim_infos['idle'].filter(func(i): return !i['is_start'] and !i['is_end'])
+			var end_actions = anim_infos['deactivate']
+			var inner_machine = create_state_machine_box(start_actions, idle_actions, end_actions, 'is_moving or is_attacking or is_jumping')
+			state_machine.add_node('idle', inner_machine)
+		elif main_anims and leave_anims and enter_anims:
+			var inner_machine = create_state_machine_box(enter_anims, main_anims, leave_anims, 'is_moving')
+			state_machine.add_node(action, inner_machine)
+		else:
+			var new_node = create_blend_space()		
+			place_points_in_blend(anim_infos[action], new_node)
+			state_machine.add_node(action, new_node)
 		
-		var directions = anim_infos[action].map(func(i): return i['direction'])
-		for direction in directions:
-			if direction == 'none':
-				var point = AnimationNodeAnimation.new()
-				point.animation = anim_info['anim_name']
-				action_blend_space.add_blend_point(point, Vector2.ZERO)
-			else:
-				var pos = BLEND_POSITIONS[direction]
-				var point = AnimationNodeAnimation.new()
-				point.animation = "%s-%s-%s" % [character, action, direction]
-				action_blend_space.add_blend_point(point, pos)
-		state_machine.add_node(action, action_blend_space)
-		# Adjust the nodes to not overlap in the AnimationTree editor
+		# Place the nodes to not overlap in the AnimationTree editor
 		if STANDARD_ACTIONS.has(action):
 			var point = get_point_on_circle(standard_nodes_index, STANDARD_ACTIONS.keys().size())
 			point += Vector2(0, 200)
@@ -108,21 +117,6 @@ static func add_standard_transitions(state_machine: AnimationNodeStateMachine, a
 			# Death is terminal, no more transition building
 			if action == 'die':
 				continue
-			var goto_idle_anim = actions.filter(func(i): return i['is_end']).front()
-			var main_anim = actions.filter(func(i): return !i['is_start'] and !i['is_end']).front()
-			var leave_idle_anim = actions.filter(func(i): return i['is_start']).front()
-			# Some monsters have idle/activate/deactivate with no explicit attribution to idle
-			if (action == 'idle') and anim_infos.keys().has('activate') and anim_infos.keys().has('deactivate'):
-				state_machine.remove_node('idle')
-				var inner_machine = create_state_machine_box("%s-%s" % [entity, 'activate'], "%s-%s" % [entity, 'idle'], "%s-%s" % [entity, 'deactivate'], 'is_moving or is_attacking or is_jumping')
-				state_machine.add_node('idle', inner_machine)
-				state_machine.add_transition("Start", action, create_immediate_transition(condition))
-			# Some have like - idle, idle-start, idle-end, even idle-downleft-start
-			if goto_idle_anim and main_anim and leave_idle_anim:
-				state_machine.remove_node(action)
-				var inner_machine = create_state_machine_box(goto_idle_anim['anim_name'], main_anim['anim_name'], leave_idle_anim['anim_name'], 'is_moving')
-				state_machine.add_node(action, inner_machine)
-				state_machine.add_transition("Start", action, create_immediate_transition(condition))
 			# Check for other actions to build typical transitions from This->Those
 			for target_action in STANDARD_ACTIONS:
 				if target_action == action:
@@ -149,34 +143,40 @@ static func create_at_end_transition(condition: String, switch_mode = AnimationN
 
 static func create_blend_space() -> AnimationNodeBlendSpace2D:
 	var blend_space: AnimationNodeBlendSpace2D = AnimationNodeBlendSpace2D.new()
-	
 	# Set up standard blend space configuration
 	blend_space.blend_mode = AnimationNodeBlendSpace2D.BLEND_MODE_DISCRETE
 	blend_space.auto_triangles = true
 	
 	return blend_space
 
-static func create_state_machine_box(start_animation: String, idle_animation: String, exit_animation: String, exit_condition: String) -> AnimationNodeStateMachine:
+static func place_points_in_blend(actions: Array, space: AnimationNodeBlendSpace2D) -> void:
+	for action in actions:
+		var direction = action['direction']
+		var point = AnimationNodeAnimation.new()
+		point.animation = action['anim_name']
+		space.add_blend_point(point, BLEND_POSITIONS[direction])
+
+static func create_state_machine_box(enter_anims: Array, main_anims: Array, exit_anims: Array, exit_condition: String) -> AnimationNodeStateMachine:
 	var inner_machine = AnimationNodeStateMachine.new()
-	var activate_blend_space = create_blend_space()
-	inner_machine.add_node("activate", activate_blend_space)
-	var deactivate_blend_space = create_blend_space()
-	inner_machine.add_node("deactivate", deactivate_blend_space)
-	var idle_blend_space = create_blend_space()
-	inner_machine.add_node("idle", idle_blend_space)
-	var start_point = AnimationNodeAnimation.new()
-	start_point.animation = start_animation
-	activate_blend_space.add_blend_point(start_point, Vector2.ZERO)
-	var idle_point = AnimationNodeAnimation.new()
-	idle_point.animation = idle_animation
-	idle_blend_space.add_blend_point(idle_point, Vector2.ZERO)
-	var exit_point = AnimationNodeAnimation.new()
-	exit_point.animation = exit_animation
-	deactivate_blend_space.add_blend_point(exit_point, Vector2.ZERO)
-	inner_machine.add_transition('Start', 'deactivate', create_immediate_transition(''))
-	inner_machine.add_transition('deactivate', 'idle', create_at_end_transition(''))
-	inner_machine.add_transition('idle', 'activate', create_immediate_transition(exit_condition))
-	inner_machine.add_transition('activate', 'End', create_at_end_transition(''))
+	
+	var transition_in_blend = create_blend_space()
+	inner_machine.add_node("transition_in", transition_in_blend)
+	inner_machine.set_node_position("transition_in", Vector2(300, 200))
+	var main_blend = create_blend_space()
+	inner_machine.add_node("main_animation", main_blend)
+	inner_machine.set_node_position("main_animation", Vector2(500, 200))
+	var transition_out_blend = create_blend_space()
+	inner_machine.add_node("transition_out", transition_out_blend)
+	inner_machine.set_node_position("transition_out", Vector2(700, 200))
+
+	place_points_in_blend(enter_anims, transition_in_blend)
+	place_points_in_blend(main_anims, main_blend)
+	place_points_in_blend(exit_anims, transition_out_blend)
+
+	inner_machine.add_transition('Start', 'transition_in', create_immediate_transition(''))
+	inner_machine.add_transition('transition_in', 'main_animation', create_at_end_transition(''))
+	inner_machine.add_transition('main_animation', 'transition_out', create_immediate_transition(exit_condition))
+	inner_machine.add_transition('transition_out', 'End', create_at_end_transition(''))
 	return inner_machine
 
 static func get_point_on_circle(index: int, total_points: int, diameter: float = 500) -> Vector2:
